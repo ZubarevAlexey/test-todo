a1ed3af388391321013d0a96bebabfa8c2de5da6
User <stparchkg@gmail.com>
2023-10-02 12:27:09 +0600

    создал пустой проект и прописал настройки для запуска пректа

---
+++ src/main/java/kg/java/testtodo/TestTodoApplication.java
@@ -0,0 +1,13 @@
+package kg.java.testtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTodoApplication.class, args);
+	}
+
+}
