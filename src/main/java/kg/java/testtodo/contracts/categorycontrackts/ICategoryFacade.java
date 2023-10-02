8d910ea0d3041f4d57e2057c8f8aee844a9d8088
User <stparchkg@gmail.com>
2023-10-02 16:26:34 +0600

    добавил facade для категории

---
+++ src/main/java/kg/java/testtodo/contracts/categorycontrackts/ICategoryFacade.java
@@ -0,0 +1,15 @@
+package kg.java.testtodo.contracts.categorycontrackts;
+
+import kg.java.testtodo.exceptions.EntityDuplicateExceptions;
+import kg.java.testtodo.exceptions.EntityNotFoundException;
+import kg.java.testtodo.models.dtos.category.*;
+import kg.java.testtodo.models.dtos.user.*;
+import org.springframework.http.HttpStatus;
+
+public interface ICategoryFacade {
+    CategoryDto get(FindByIdCategoryDto model) throws EntityNotFoundException;
+    CategoryDto register(CreatedCategoryDto model) throws EntityDuplicateExceptions;
+    CategoryDto update(UpdateCategoryDto model) throws EntityDuplicateExceptions,EntityNotFoundException;
+
+    HttpStatus delete(DeleteCategoryDto model) throws EntityNotFoundException;
+}
