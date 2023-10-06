a5d558e66490a42cbb73da8394d1805abb6ea70e
User <stparchkg@gmail.com>
2023-10-06 13:07:39 +0600

    создал уровень абстракции для таск сервира

---
+++ src/main/java/kg/java/testtodo/core/contracts/services/ITaskService.java
@@ -0,0 +1,22 @@
+package kg.java.testtodo.core.contracts.services;
+
+import kg.java.testtodo.core.exceptions.EntityDuplicateExceptions;
+import kg.java.testtodo.core.exceptions.EntityNotFoundException;
+import kg.java.testtodo.core.models.dtos.category.*;
+import kg.java.testtodo.core.models.dtos.task.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public interface ITaskService {
+    TaskDto add(CreatedTaskDto model) throws EntityDuplicateExceptions, EntityNotFoundException;
+
+    TaskDto update(UpdatedTaskDto model) throws EntityNotFoundException, EntityDuplicateExceptions;
+
+    HttpStatus delete(DeleteTaskDto model) throws EntityNotFoundException;
+
+    TaskDto getById(FindByIdTaskDto model) throws EntityNotFoundException;
+}
